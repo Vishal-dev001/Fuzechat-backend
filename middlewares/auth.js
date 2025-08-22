98228eae296116e5149195cb008d3b214bf5c786
vishal gupta <vishalgupta989974@gmail.com>
2025-08-22 13:53:06 +0530

    basic done

---
+++ middlewares/auth.js
@@ -0,0 +1,47 @@
+import { CHAT_TOKEN } from "../constants/config.js";
+import { User } from "../models/user.js";
+import { ErrorHandler } from "../utils/utility.js";
+import { TryCatch } from "./error.js";
+import jwt from "jsonwebtoken";
+
+const isAuthenticated = TryCatch(async(req,res,next) =>{
+
+    const token = req.cookies["chat-token"];
+
+    if(!token){
+        return next(new ErrorHandler("Please login to access this route",401));
+    }
+
+    const decodedData = jwt.verify(token,process.env.JWT_SECRET);
+
+    req.user = decodedData._id;
+
+    next();
+})
+
+const socketAuthenticator = async (err, socket, next) => {
+    try {
+      if (err) return next(err);
+  
+      const authToken = socket.request.cookies[CHAT_TOKEN];
+  
+      if (!authToken)
+        return next(new ErrorHandler("Please login to access this route", 401));
+  
+      const decodedData = jwt.verify(authToken, process.env.JWT_SECRET);
+  
+      const user = await User.findById(decodedData._id);
+  
+      if (!user)
+        return next(new ErrorHandler("Please login to access this route", 401));
+  
+      socket.user = user;
+  
+      return next();
+    } catch (error) {
+      console.log(error);
+      return next(new ErrorHandler("Please login to access this route", 401));
+    }
+  };
+
+export {isAuthenticated,socketAuthenticator}
